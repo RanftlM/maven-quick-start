99c3579b4d1a422ff2c7eaada968f7c6e2f96854
Michael Ranftl <Michael.Ranftl@gmail.com>
2020-11-23 14:21:37 +0100

    USe Stringutils and add dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,33 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
-    
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words," ");
+		return (separateWords == null)?0:separateWords.length;
+	}
+    public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hallo");
+		for (String greeting:greetings){
+			System.out.println("Greeting: "+greetings);
+		}
+		
+	}
     public Application() {
         System.out.println ("Inside Application");
+	
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+		app.greet();
+		int count = app.countWords(" i have four words");
+		System.out.println("Word Count: "+ count);
     }
 }
